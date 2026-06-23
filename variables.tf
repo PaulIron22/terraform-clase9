ccda5c4af887810fc844e0a5d26f49f0fb76a4ec
PaulIron22 <pablo7_f@hotmail.com>
2026-06-23 20:35:31 -0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# variables.tf - Declaración de todas las variables del proyecto
+variable "location" {
+  description = "Región de Azure donde se crean los recursos"
+  type        = string
+  default     = "eastus2"
+}
+
+variable "prefix" {
+  description = "Prefijo para nombrar los recursos. Usar tu apellido."
+  type        = string
+  # Cambiar 'tuapellido' por tu apellido real
+  default = "Fierro"
+}
